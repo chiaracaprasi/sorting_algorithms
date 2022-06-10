ede58d8d6c38ec98ab8d5a7228f9402bab297d8c
Chiara Caprasi <caprasichiara@gmail.com>
2022-06-09 21:55:37 -0700

    build: add 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - swap two int pointers
+ * @xp: int to swap
+ * @yp: int to swap
+ * Return: void
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * bubble_sort - sorts array of integers in ascending order using Bubble sort
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
+				swap(&array[j], &array[j + 1]);
+		print_array(array, size);
+	}
+}
