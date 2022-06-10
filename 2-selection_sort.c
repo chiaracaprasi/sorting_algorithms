2cab107cac2782859db939301d34610ee13fa466
Chiara Caprasi <caprasichiara@gmail.com>
2022-06-10 01:51:27 -0700

    build: add 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - swap two int pointers
+ * @xp: int to swap
+ * @yp: int to swap
+ * Return: void
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * selection_sort - sorts array of int in ascending order using Selection sort
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min_index;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_index])
+				min_index = j;
+
+		print_array(array, size);
+		swap(&array[min_index], &array[i]);
+
+	}
+}
