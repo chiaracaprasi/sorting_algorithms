792a45d2a63e12efa1c9c3d0d243cabe4a25f32d
jashoney <4479@holbertonschool.com>
2022-06-14 10:31:48 +1000

    fixed swap nodes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -21,21 +21,21 @@ int count_list_length(listint_t *list)
 
 /**
  * swap_nodes - swaps two nodes in a linked list
- * @current: the first node
- * @next: the other node
+ * @n1: the first node
+ * @n2: the other node
  * Return: is void
  */
 
-void swap_nodes(listint_t *current, listint_t *next)
+void swap_nodes(listint_t *n1, listint_t *n2)
 {
-	if (current->prev != NULL)
-		(current->prev)->next = next;
-	next->prev = current->prev;
-	current->prev = next;
-	current->next = next->next;
-	next->next = current;
-	if (current->next != NULL)
-		(current->next)->prev = current;
+	if (n1->prev != NULL)
+		(n1->prev)->next = n2;
+	if (n2->next != NULL)
+		(n2->next)->prev = n1;
+	n2->prev = n1->prev;
+	n1->prev = n2;
+	n1->next = n2->next;
+	n2->next = n1;
 }
 
 /**
